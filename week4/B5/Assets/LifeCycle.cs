56a0468dd360529a993b8ec58c5a8cd9134e5be4
polaris65b <42924432+polaris65b@users.noreply.github.com>
2023-04-12 17:35:29 +0900

    B5
    초기화 영역 Awake(), Start()

--- week4/B5/Assets/LifeCycle.cs
+++ week4/B5/Assets/LifeCycle.cs
@@ -9,4 +9,10 @@ public class LifeCycle : MonoBehaviour
     {
         Debug.Log("플레이어 데이터가 준비되었습니다.");
     }
+
+    // Start : 업데이트 시작 직전, 최초 실행
+    private void Start()
+    {
+        Debug.Log("사냥 장비를 챙겼습니다.");
+    }
 }
