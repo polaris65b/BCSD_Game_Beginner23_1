02c2e530d2d06abec22e755a56f38069e3c3ba22
polaris65b <42924432+polaris65b@users.noreply.github.com>
2023-04-12 18:04:26 +0900

    Input.anykey

--- week4/B5/week4/Assets/NewBehaviourScript.cs
+++ week4/B5/week4/Assets/NewBehaviourScript.cs
@@ -9,5 +9,8 @@ public class NewBehaviourScript : MonoBehaviour
         // Input : 게임 내 입력을 관리하는 클래스
         if (Input.anyKeyDown)
             Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+
+        if (Input.anyKey)
+            Debug.Log("플레이어가 아무 키를 누르고 있습니다.");
     }
 }
