d53415c78a2cd7d0d8383cec6bd6d67bb65ec8bf
polaris65b <42924432+polaris65b@users.noreply.github.com>
2023-04-12 17:38:48 +0900

    B5
    물리 FixedUpdate()

--- week4/B5/Assets/LifeCycle.cs
+++ week4/B5/Assets/LifeCycle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LifeCycle : MonoBehaviour
 {
+    //초기화
+
     // Awake : 게임 오브젝트 생성할 때, 최초 실행
     void Awake()
     {
@@ -14,5 +16,14 @@ public class LifeCycle : MonoBehaviour
     private void Start()
     {
         Debug.Log("사냥 장비를 챙겼습니다.");
+    }
+
+    // 물리
+
+    //FixedUpdate : 물리 연산 업데이트
+    // 고정된 실행 주기로 CPU를 많이 사용.
+    private void FixedUpdate()
+    {
+        Debug.Log("이동~");
     }
 }
